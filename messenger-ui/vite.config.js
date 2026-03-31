28148de5a509dd3d9d1015a2792449e030b52a8b
hnk <hnkim@wezlesoft.co.kr>
2026-03-31 11:07:12 +0900

    Fixed vite.config

--- messenger-ui/vite.config.js
+++ messenger-ui/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/hk/api': {
+      '/api': {
         target: 'http://localhost:8085',
         changeOrigin: true,
         secure: false,
